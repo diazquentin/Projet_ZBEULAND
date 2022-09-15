93fc0cecbe9bc6acc483a5a2e876d96aca99399b
Quentin Diaz <qd@sylob.com>
2022-09-15 15:40:33 +0200

    page d'accueil

---
+++ function.js
@@ -0,0 +1,14 @@
+const BASE_URL = "https://fhir.alliance4u.io/api/"
+
+function getPractitioner() {
+    $.get(BASE_URL +"practitioner", function (result) {
+        for(let i=0; i < result.length; i++){
+            let clone = $( "#template-practitioner" ).clone();
+            if(result[i].name[0]) clone.find(".name").text(result[i].name[0].text);
+            if(result[i].telecom && result[i].telecom[0]) clone.find(".telecom").text(result[i].telecom[0].value);
+            clone.appendTo( "#list-practitioner" );
+        }
+        $( "#template-practitioner" ).remove();
+    })
+}
+
