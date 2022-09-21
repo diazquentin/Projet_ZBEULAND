75e5417a8d404a6bd3d4cc202e2ec6e17d130500
Quentin Diaz <qd@sylob.com>
2022-09-21 09:40:18 +0200

    consultation de rdv

--- function.js
+++ function.js
@@ -20,3 +20,65 @@ function getNbPractitioner() {
     })
 }
 
+
+new Vue({
+
+    el: '#principal',
+  
+    data: {
+      contacto: {id: '', nombre: '', apellidos: '', fnacimiento: '', telefono: '' },
+      agenda: [],
+      contador: 0
+    },
+  
+    mounted: function() {
+      this.cargarAgenda();
+    },
+  
+    methods: {
+      cargarAgenda: function() {
+        var agendaDatos = [
+          {
+            id: 1,
+            nombre: 'Mario',
+            apellidos: 'Brito Morales',
+            fnacimiento: '19/01/1973',
+            telefono: '699800461'
+          },
+          {
+            id: 2,
+            nombre: 'Juana',
+            apellidos: 'Rodríguez  Pérez',
+            fnacimiento: '29/11/1970',
+            telefono: '659855461'
+          },
+          {
+            id: 3,
+            nombre: 'Vicente',
+            apellidos: 'Roca Valido',
+            fnacimiento: '22/07/1983',
+            telefono: '698554781'
+          }
+        ];
+  
+        this.agenda = agendaDatos;
+        this.contador=agendaDatos.length;
+      },
+  
+      addContacto: function() {
+        if(this.contacto.nombre) {
+          this.contador++;
+          this.contacto.id=this.contador;
+          this.agenda.push(this.contacto);
+          this.contacto = { nombre: '', apellidos: '', fnacimiento: '', telefono: '' };
+        }
+      },
+  
+      borrarContacto: function(index) {
+        if(confirm("Etes vous sûr d'annuler la prise de rdv ?")) {
+          this.agenda.splice(index, 1);        
+        }
+      }
+    }
+  });
+  
